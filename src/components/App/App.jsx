07d71ad3f1d2e0c887c11f2938dd7dadafabce7b
ilya9293 <ilya9293@ukr.net>
2022-06-01 13:40:43 +0300

    add Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,15 @@
+import Profile from 'components/Profile';
+import user from '../../data/user.json';
+
 const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
 
