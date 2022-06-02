895a61fc0f1ca6d7a0e287e240b77dfc13beb7bb
Ilya Gomonets <ilya9293@ukr.net>
2022-06-02 16:35:28 +0300

    add statistics

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,20 @@
 import Profile from 'components/Profile';
+import Statistics from 'components/Statistics';
 import user from '../../data/user.json';
+import data from '../../data/data.json';
 
 const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
 
